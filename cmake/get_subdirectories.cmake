de83fdc157a53098e18009467a43ac7a494d250e
tymmkang <tymmkang@gmail.com>
2019-04-19 17:17:49 +0900

    Add folder for CMake reusability
     - Add CMakeLists.txt for include *.cmake files
     - Add get_subdirectories.cmake
     - Add source_grouping.cmake

---
+++ cmake/get_subdirectories.cmake
@@ -0,0 +1,15 @@
+# Get all subdirectories in specific directory
+# curdir[in]        Specific directory to get subdirectories
+# result[out]       Return parameter
+macro(NB_MACRO_GET_SUBDIRECTOYIES curDir result)
+file(GLOB children RELATIVE ${curDir} ${curDir}/*)
+
+set(dirList)
+foreach(child ${children})
+  if(IS_DIRECTORY ${curDir}/${child})
+      set(dirList ${dirList} ${child})
+  endif()
+endforeach()
+
+set(${result} ${dirList})
+endmacro()
---
+++ cmake/source_grouping.cmake
@@ -0,0 +1,31 @@
+# Source grouping for organization in IDE
+
+# Fix path for source grouping
+# pathToFix[in]         Path string to fix
+# fixedPath[out]        Fixed path
+function(NB_FUNC_FIX_PATH_FOR_GROUP_SOURCE pathToFix fixedPath)
+    set(ret ${pathToFix})
+
+    if(MSVC)
+        string(REPLACE "/" "\\" ret ${pathToFix})
+    endif()
+
+    set(${fixedPath} ${ret} PARENT_SCOPE)
+endfunction()
+
+# Group source
+# sourcesToGroup[in]    Path string to group
+# groupRoot[in]         Group root path string
+function(NB_FUNC_GROUP_SOURCE sourcesToGroup groupRoot)
+    foreach(source ${sourcesToGroup})
+        file(RELATIVE_PATH relative ${groupRoot} ${source})
+        get_filename_component(parentDir ${relative} DIRECTORY)
+
+        if(NOT ${parentDir} STREQUAL "")
+            NB_FUNC_FIX_PATH_FOR_GROUP_SOURCE(${parentDir} fixedParentdir)
+            source_group(${fixedParentdir} FILES ${source})
+        else()
+            source_group("" FILES ${source})
+        endif()
+    endforeach()
+endfunction()
