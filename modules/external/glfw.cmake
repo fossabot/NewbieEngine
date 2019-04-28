40cbf631cc044bdbebc09273c2375a41f3f0f9c7
tymmkang <tymmkang@gmail.com>
2019-04-28 19:00:44 +0900

    Add external libraries
     - Add GLFW
     - Add imgui
     - Add dependency to core module with GLFW

---
+++ modules/external/glfw.cmake
@@ -0,0 +1,31 @@
+# GLFW
+# Official website : https://www.glfw.org/
+# Git repository : https://github.com/glfw/glfw
+
+ExternalProject_Add( glfw PREFIX glfw
+    GIT_REPOSITORY https://github.com/glfw/glfw.git
+    GIT_TAG 3.3
+
+    INSTALL_DIR "${CMAKE_SOURCE_DIR}/modules/external/glfw"
+    
+    # Skip build command.
+    BUILD_COMMAND ""    
+
+    CMAKE_ARGS
+        "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+        "-DCMAKE_BUILD_TYPE=Release"
+        "-DCMAKE_CONFIGURATION_TYPES=Release"
+        "-DGLFW_BUILD_EXAMPLES=OFF"
+        "-DGLFW_BUILD_TESTS=OFF"
+        "-DGLFW_BUILD_DOCS=OFF"
+
+    INSTALL_COMMAND
+        ${CMAKE_COMMAND}
+        --build .
+        --target install
+        --config Release
+    
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+set_target_properties(glfw PROPERTIES FOLDER externals)
---
+++ modules/external/imgui.cmake
@@ -0,0 +1,27 @@
+# imgui
+# Git repository : "https://github.com/ocornut/imgui
+
+ExternalProject_Add( imgui PREFIX imgui
+    GIT_REPOSITORY "https://github.com/ocornut/imgui.git"
+    GIT_TAG "655ebe4eaff15a3996233f81885e1a3e218caab0"
+
+    INSTALL_DIR "${CMAKE_SOURCE_DIR}/modules/external/imgui"
+    
+    # Skip build command.
+    BUILD_COMMAND ""
+
+    # Skip configure command.
+    CONFIGURE_COMMAND ""
+
+    INSTALL_COMMAND
+        ${CMAKE_COMMAND} -E copy_directory  
+        "${CMAKE_BINARY_DIR}/modules/external/imgui/src/imgui" 
+        "${CMAKE_SOURCE_DIR}/modules/external/imgui/include/imgui"
+
+    CMAKE_ARGS
+        "-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>"
+
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+set_target_properties(imgui PROPERTIES FOLDER externals)
