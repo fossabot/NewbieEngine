4057225670a6be08b2f04b82d47f1afacbd042fd
tymmkang <tymmkang@gmail.com>
2019-05-17 07:38:07 +0900

    Add 'NB_FUNC_GROUP_SOURCE_WITH_TAIL' function to grouping source and additional tail group

--- CMake/SourceGrouping.cmake
+++ CMake/SourceGrouping.cmake
@@ -29,3 +29,21 @@ function(NB_FUNC_GROUP_SOURCE sourcesToGroup groupRoot)
         endif()
     endforeach()
 endfunction()
+
+# Group source
+# sourcesToGroup[in]    Path string to group
+# groupRoot[in]         Group root path string
+# groupTail[in]            Additional tail group
+function(NB_FUNC_GROUP_SOURCE_WITH_TAIL sourcesToGroup groupRoot groupTail)
+    foreach(source ${sourcesToGroup})
+        file(RELATIVE_PATH relative ${groupRoot} ${source})
+        get_filename_component(parentDir ${relative} DIRECTORY)
+
+        if(NOT ${parentDir} STREQUAL "")
+            NB_FUNC_FIX_PATH_FOR_GROUP_SOURCE("${groupTail}/${parentDir}" fixedParentdir)
+            source_group(${fixedParentdir} FILES ${source})
+        else()
+            source_group("${groupTail}" FILES ${source})
+        endif()
+    endforeach()
+endfunction()
